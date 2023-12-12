c70b102fe97697ea72d8012bc4049366787cf511
低耐久的铁砧 <67966070+Ironnoob73@users.noreply.github.com>
2023-12-12 13:37:50 +0800

    20231212DU2

--- Title/Saves.gd
+++ Title/Saves.gd
@@ -1,26 +1,40 @@
 extends HBoxContainer
 
 @onready var list = $List
-@onready var file_name = $Preview/Name
+@onready var file_path = DirAccess.open(Global.DATA_PATH)
+@onready var file_name_label = $Preview/Name
+@onready var file_cover = $Preview/Cover
+@onready var file_discription = $Preview/Discription
+
+var broken_file_image = "res://Resources/Image/broken_file.svg"
+var unknown_file_image = "res://Resources/Image/unknown_file.svg"
+#var current_file_path = null
 
 func _ready():
 	refresh()
 func refresh():
 	list.clear()
-	var dir = DirAccess.open(Global.DATA_PATH)
-	if dir:
-		dir.list_dir_begin()
-		var file_name = dir.get_next()
+	if file_path:
+		file_path.list_dir_begin()
+		var file_name = file_path.get_next()
 		while file_name != "":
-			if dir.current_is_dir():
+			if file_path.current_is_dir():
 				print("发现目录：" + file_name)
 				list.add_item(file_name)
 			else:
 				print("发现文件：" + file_name)
 				list.set_item_custom_fg_color(list.add_item(file_name),Color(0.75,0.75,0.75))
-			file_name = dir.get_next()
+			file_name = file_path.get_next()
 	else:
 		print("尝试访问路径时出错。")
 
 func _on_list_item_selected(index):
-	file_name.text = list.get_item_text(index)
+	file_name_label.text = list.get_item_text(index)
+	var current_file_path = Global.DATA_PATH + "\\" + file_name_label.text
+	if DirAccess.open(current_file_path):
+		file_discription.text = "AVALIABLE"
+		file_cover.texture.load(current_file_path + "\\cover.png")
+	else:
+		file_discription.text = "[color=red]NOT AVALIABLE[/color]"
+		file_cover.texture.load(broken_file_image)
+	print(DirAccess.open(current_file_path))
